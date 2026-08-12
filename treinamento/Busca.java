b428a3723fb25b8668f90af4c86f618c74312b40
Paula Moschioni <paulamoschioni2007@gmail.com>
2026-08-12 18:55:08 -0300

    Atualizacao: 2026-08-12 18:55

---
+++ treinamento/Busca.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Busca{
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+    // Ler numeros
+    int tamVet, num;
+    tamVet = sc.nextInt();
+    int[] vet = new int[tamVet];
+    // Guardando os numeros no vetor
+    for(int i = 0; i < tamVet; i++){
+    num = sc.nextInt();
+    vet[i] = num;
+    }
+    // Realizando a busca
+    int menor = vet[0], maior = vet[0];
+    
+    for(int j = 0; j < tamVet; j++){
+            if(vet[j]< menor) menor = vet[j];
+            if(vet[j] > maior) maior = vet[j];
+        
+
+    }
+    System.out.println("Menor: "+ menor + "| Maior: "+ maior);
+
+
+
+ sc.close(); } 
+
+
+}
