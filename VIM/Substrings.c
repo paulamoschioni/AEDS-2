7d63618ec99f88fe81693a5dc9a76f7960d726f8
Paula Moschioni <paulamoschioni2007@gmail.com>
2026-08-21 17:35:40 -0300

    .
    .

---
+++ VIM/Substrings.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*Autora: Paula Moschioni
+ Crie um metodo que retorna o tamanho da maior substring sem caracteres repetidos*/
+int SubstringLonga(){
+
+}
+
+int main(){
+	//Recebendo varias strings
+	char frase[100];
+	scanf("%s",frase);
+
+	while(frase[0] != 'F' || frase[1] != 'I' || frase[2] != 'M'){
+	
+	}
+return 0;
+}
