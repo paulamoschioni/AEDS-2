7d8e1c38fef806eaf12de8909d60a95aa89dc5c1
Paula Moschioni <paulamoschioni2007@gmail.com>
2026-09-08 18:35:58 -0300

    unfinished activity

---
+++ trabalho pratico 2/Modelagem.java
@@ -0,0 +1,179 @@
+import java.util.Scanner;
+class Modelagem{
+	public int BuscaSequencial(Veiculo v[], int ident){
+	int pos = -1;
+	int tam = v.length;
+	
+	for(int i = 0; i < tam; i++){
+		if(v[i].getId() == ident) {
+		pos = i;
+		}
+	   }
+		return pos;
+	}
+
+	public class Veiculo[] LeitorCsv(String caminhoArquivo){
+	//guardar no vetor v cada linha ate o \n
+	//chamar parseVeiculo
+
+
+	}
+
+	public class Data{
+	//atributos
+	private int dia;
+	private int mes;
+	private ano;
+	//metodos
+	public static Data parseData(String s){	   //recebe no formato AAAA-MM-DD
+	String[] d = s.split("-");
+	ano = Integer.parseInt(x[0]);
+	mes = Integer.parseInt(x[1]);
+	dia = Integer.parseInt(x[2]);
+	}
+	
+	public static String format(){
+	return String.format("%02d/%02d/%04d",dia,mes,ano);
+	}
+       }
+
+	public class Veiculos{
+	//atributos
+	private int id;
+	private String marca;
+	private String modelo;
+	private int ano;
+	private String categoria;
+	private String[] combustivel;
+	private int cilindros;
+	private double cilindrada;
+	private String transmissao;
+	private String tracao;
+	private	double consumoCidade;
+	private double consumoEstrada;
+	private double co2;
+	private boolean turbo;
+	private Data dataRegistro;
+
+	//metodos
+	public Veiculo(int i, int ma, int mo, int a, String cat, String[] comb, int co, double ca, String t, String tr, double cC, double cE, double c2, boolean t, Data dt){
+	id = i;
+	marca = m;
+	modelo = mo;
+	ano = a;
+	categoria = cat;
+	combustivel = comb;
+	cilindros = co;
+	cilindrada = ca;
+	transmissao = t;
+	tracao = tr;
+	consumoCidade = cC;
+	consumoEstrada = cE;
+	co2 = c2;
+	turbo = t;
+	dataRegistro = dt;
+	}
+
+	public int getId(){
+	return id;
+	}
+	public String getMarca(){
+	return marca;
+	}
+	public String getCategoria(){
+	return categoria;
+	}
+	public int getAno(){
+	return ano;
+	}	
+	public String[] getCombustivel(){
+	return combustivel;
+	}
+	public int getCilindros(){
+	return cilindros;
+	}
+	public double getCilindrada(){
+	return cilindrada;
+	}
+	public String getTracao(){
+	return tracao;
+	}
+	public String getTransmissao(){
+	return transmissao;
+	}
+	public double getConsumoCidade(){
+	return consumoCidade;
+	}
+	public double getConsumoEstrada(){
+	return consumoEstrada;
+	}
+	public double getCo2(){
+	return co2;
+	}
+	public boolean getTurbo(){
+	return turbo;
+	}
+	public Data getDataregistro(){
+	return dataRegistro;
+	}
+
+	public static Veiculo parseVeiculo(String s){	//cria um novo veiculo com 15 atributos e retorna ele				
+	String[] x = s.split(",");		//cria vetor de 15 strings p guardar os 15 atributos. Split retorna automaticamente string[]
+	//atribuindo valores a partir da string[] gerada
+	//é preciso colocar os tipos dos atributos dnv, pois como o obj Veiculo nao existe ainda, o programa nao sabe qual e o tipo
+	int id = Integer.parseInt(x[0]);				
+	String marca = x[1];
+	String modelo = x[2];
+	int ano = Integer.parseInt(x[3]);
+	String categoria = x[4];
+	String[] combustivel = x[5].split(";");
+	int cilindros = Integer.parseInt(x[6]);
+	double cilindrada = Double.parseDouble(x[7]);
+	String transmissao = x[8];
+	String tracao = x[9];
+	double consumoCidade = Double.parseDouble(x[10]);
+	double consumoEstrada = Double.parseDouble(x[11]);
+	double co2 = Double.parseDouble(x[12]);
+	boolean turbo = Boolean.parseBoolean(x[13]);
+	Data dataRegistro = Data.parseData(x[14]);
+
+	return new Veiculo(id, marca, modelo, ano, categoria, combustivel, cilindros, cilindrada, transmissao, tracao, consumoCidade, consumoEstrada, co2,turbo,dataRegistro);  //criando um novo objeto da classe Veiculo, chamando seu construtor
+	}
+
+	public String format(){
+	return String.format("[%d ## %s ## %s ## %d ## %s ## [%s] ## %d ## %.1f ## %s ## %s ## %.2f ## %.2f ## %.1f ## %s]",id, marca, modelo, ano, categoria, String.join(";",combustivel), cilindros, cilindrada, transmissao, tracao, consumoCidade, consumoEstrada, co2,turbo,dataRegistro.format());
+	}
+	  }
+
+
+
+
+
+
+
+		
+
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	//ler CSV
+	
+
+	//Ler ids para buscar em Veiculo[]
+	int num = sc.nextInt();
+	int resultado;
+	while(num != -1){	//le conjunto de ids para buscar
+	//busca pelo id num
+	resultado = BuscaBinaria(v,num);
+		
+	if(resultado > -1){
+	System.out.println(v[resultado].format());
+		
+		
+		
+		
+		
+		
+	num = sc.nextInt();
+	}
+	sc.close();}
+}
